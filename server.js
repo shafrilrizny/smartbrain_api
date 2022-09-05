675a1858cbff4863666fb976be8e77f69f46b8e0
shafrilrizny <riznyapp@gmail.com>
2022-09-05 15:41:57 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcrypt-nodejs');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signIn = require('./controllers/signIn');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex ({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'rizzney',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send(database.users);
+})
+
+app.post('/signIn', (req, res) => {
+  signIn.handleSignIn (req, res, db, bcrypt)
+})
+
+app.post('/register', (req, res) => { 
+  register.handleRegister (req, res, db, bcrypt)
+})
+
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet (req, res, db)
+})
+
+app.put('/image', (req, res) => {
+  image.handleImage (req, res, db)
+})
+
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall (req, res)
+})
+
+const PORT = process.env.PORT
+app.listen(3005)
