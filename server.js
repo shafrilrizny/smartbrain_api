4708db9f39d8fd8f0d769a953befbe0ba5435599
shafrilrizny <riznyapp@gmail.com>
2022-09-06 11:45:41 +0800

    update package.json

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const signIn = require('./controllers/signIn');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
-
 const db = knex ({
   client: 'pg',
   connection: {
